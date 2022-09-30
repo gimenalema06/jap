cf8cba0f9083db6e91855eb4e666c5c49bdb13eb
gimenalema06 <109124309+gimenalema06@users.noreply.github.com>
2022-09-30 00:08:46 -0300

    dropdown menu improvement

--- js/product-info.js
+++ js/product-info.js
@@ -122,23 +122,43 @@ document.getElementById("comment").addEventListener("click" , function(){
 if (localStorage.getItem("user")) {
     let htmlContentToAppend = "";
     htmlContentToAppend += `
-    <a class="nav-link">`+localStorage.getItem("user")+ `</a>
-    
+    <div class="dropdown">
+    <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="background-color:dark-grey ; color:white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+        </svg>
+        &nbsp ${localStorage.getItem("user")}
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
+        <li><a class="dropdown-item" href="cart.html"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                    <path
+                        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13
+                         12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 
+                         12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1
+                          1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg> Carrito</a></li>
+        <li><a class="dropdown-item" href="#" id="remove_user"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 
+                        1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z" />
+                    <path fill-rule="evenodd"
+                        d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5
+                         0 0 1-.708.708l-3-3z" />
+                </svg> Cerrar sesión</a></li>
+    </ul>
+    </div>  
     `
     document.getElementById("User_place").innerHTML = htmlContentToAppend;
-    htmlContentToAppend=`
-    <button type="button" style="background-color: black" id="remove_user">
-        &#10060
-    </button>`
-    document.getElementById("logout").innerHTML = htmlContentToAppend;
-
     document.getElementById('remove_user').addEventListener("click", function(){
         let htmlContentToAppend = "";
         htmlContentToAppend = `
         <a class="nav-link" href="login.html">Ingresar</a>
         `
         document.getElementById("User_place").innerHTML = htmlContentToAppend;
-        document.getElementById("logout").innerHTML = "";
         localStorage.removeItem("user"); 
     })
 }
