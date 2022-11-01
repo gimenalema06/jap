b36fae986bd52180fb9da4879ac417a56df21f3e
gimenalema06 <109124309+gimenalema06@users.noreply.github.com>
2022-11-01 12:18:13 -0300

    desafiate 6

--- js/cart.js
+++ js/cart.js
@@ -13,6 +13,7 @@ fetch(CART_INFO_URL + '25801' + EXT_TYPE)
             <th scope="col">Costo</th>
             <th scope="col">Cantidad</th>
             <th scope="col">Subtotal</th>
+            <th scope="col"></th>
             </tr>
         </thead>
         <tbody id="elements">
@@ -33,18 +34,21 @@ if (localStorage.length > 3) {
                 .then(res => res.json())
                 .then(data2 => {
                     htmlContentToAppend = `
-                            <tr>
+                            <tr id="${localStorage.key(i)}">
                             <td scope="row"><img src="${data2.images[0]}" class="img-thumbnail" style="max-width: 50%"></td>
                             <td>${data2.name}</td>
                             <td>${data2.currency + ` ` + data2.cost}</td>   
                             <td><input value="1" type="number" min="0" id="cant`+ i + `" onchange="subtotalArt(${localStorage.getItem(localStorage.key(i))}, ${i}); sumOfSubtotals(); shippingCostAndTotal()" ></td>
                             <td id="subtotal`+ i + `" class="cartElement">${data2.currency + ` ` + data2.cost}</td>
+                            <td id="eliminar">
+                            <button onclick="deleteProduct(${localStorage.key(i)})">
+                            <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                            </button>
+                            </td>
                             </tr>
                             `
                     document.getElementById("elements").innerHTML += htmlContentToAppend;
                     sumOfSubtotals();
-
-
 
                 })
 
@@ -53,6 +57,13 @@ if (localStorage.length > 3) {
 
     }
 
+}
+
+//funcion para el desafiate 6
+function deleteProduct(product){
+    console.log(product);
+    product.remove();
+    localStorage.removeItem(product.id);
 }
 
 
@@ -68,6 +79,7 @@ function showProdInCart(Articles) {
             <td>${articulo.currency + ` ` + articulo.unitCost}</td>   
             <td><input value="1" type="number" min="0" id="cant" onchange="subtotal(); sumOfSubtotals(); shippingCostAndTotal();"></td>
             <td id="subtotal" class="cartElement">${articulo.currency + ` ` + articulo.unitCost}</td>
+            <td></td>
         </tr>
         `
 
